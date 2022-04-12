7c5b4b5689485230bbe2a18331d40ab56df79fc4
Xavier <dejuan.xavier@gmail.com>
2022-04-12 15:43:46 +0200

    arrangement dans style

--- www/models/StyleModel.php
+++ www/models/StyleModel.php
@@ -81,6 +81,15 @@ class StyleModel extends Database
     );
   }
 
+  public function getTitleStyle($musiqueID) {
+      return $this->getMany(
+    "SELECT m.id, m.nom, m.durée, m.album, CONCAT(s.nom, '/', s.type) as style
+      FROM musique m
+      INNER JOIN style s ON s.id = m.style_id
+      ORDER BY m.nom ASC LIMIT $offset, $limit"
+    );
+  }
+
   public function styleMusique($styleID)
   {
     return $this-> getSingle(
