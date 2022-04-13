56dbadc21e9fa7fc9a75589e0075325699834fb1
pvavrina <patrick@club-linux.ch>
2022-04-13 16:48:00 +0200

    Update routes.php
    UPDATE

--- www/routes.php
+++ www/routes.php
@@ -1,14 +1,16 @@
 <?php
 
-// ---- TODO : Commenter ce bout de code, qu'est-ce qu'il recherche ? ----
+// Appel aux controlleurs
 require_once __DIR__ . "/controllers/BaseController.php";
 require_once __DIR__ . "/controllers/ArtisteController.php";
 require_once __DIR__ . "/controllers/MusiqueController.php";
 require_once __DIR__ . "/controllers/StyleController.php";
 require_once __DIR__ . "/controllers/ArtisteMusiqueController.php";
 
-// ---- TODO : Commenter ce bout de code ----
+// Liste des URI
 $routes = [
+
+  // Artistes
   "/api/artistes/list" => ['GET', 'ArtisteController', 'getList'],
   "/api/artistes/get" => ['GET', 'ArtisteController', 'get'],
   "/api/artistes/add" => ['POST', 'ArtisteController', 'store'],
@@ -16,13 +18,15 @@ $routes = [
   "/api/artistes/remove" => ['DELETE', 'ArtisteController', 'destroy'],
   "/api/artiste_groupe_musique/get" => ['GET', 'ArtisteController', 'getArtisteGroupeMusique'],
 
+  // Musique
   "/api/musique/list" => ['GET', 'MusiqueController', 'getList'],
   "/api/musique/get" => ['GET', 'MusiqueController', 'get'],
   "/api/musique/artistetitle" => ['GET', 'MusiqueController', 'getArtisteTitle'],
   "/api/musique/add" => ['POST', 'MusiqueController', 'store'],
   "/api/musique/update" => ['PUT', 'MusiqueController', 'update'],
   "/api/musique/remove" => ['DELETE', 'MusiqueController', 'destroy'],
-  
+
+  // Style
   "/api/style/list" => ['GET', 'StyleController', 'getList'],
   "/api/style/get" => ['GET', 'StyleController', 'get'],
   "/api/style/add" => ['POST', 'StyleController', 'store'],
@@ -31,6 +35,7 @@ $routes = [
   "/api/titlestyle/list" => ['GET', 'StyleController', 'getTitleStyle' ],
   "/api/stylemusique/get" => ['GET', 'StyleController', 'getStyleMusique' ],
 
+  // Artiste Musique
   "/api/artistemusique/list" => ['GET', 'ArtisteMusiqueController', 'getAllArtisteMusique' ],
   "/api/artistemusique/update" => ['PUT', 'ArtisteMusiqueController', 'updateArtisteMusique' ],
   "/api/artistemusique/delete" => ['DELETE', 'ArtisteMusiqueController', 'deleteArtisteMusique' ],
