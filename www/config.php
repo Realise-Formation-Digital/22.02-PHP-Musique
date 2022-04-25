fee553a20714cad989d00d7366e60e988ad0982b
pvavrina <patrick@club-linux.ch>
2022-04-25 14:12:51 +0200

    Update config.php

--- www/config.php
+++ www/config.php
@@ -1,10 +1,10 @@
 <?php
 
-  define("DB_HOST", "192.168.150.11");
-  define("DB_USERNAME", "root");
-  define("DB_PASSWORD", "musique");
-  define("DB_DATABASE_NAME", "musique");
-  define("DB_PORT", "43306");
+  define("DB_HOST", "ad9s9.myd.infomaniak.com");
+  define("DB_USERNAME", "ad9s9_patux");
+  define("DB_PASSWORD", "P@tr1ckV1973");
+  define("DB_DATABASE_NAME", "ad9s9_webdev");
+  define("DB_PORT", "3306");
 
 
   // define("DB_HOST", "localhost");
